a0e47a02ad3dbdd174952fd64964df2a2391ada2
Tomas <hacker-shade@seznam.cz>
2018-06-07 19:25:52 +0200

    First commit

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Raketa = new System.Windows.Forms.PictureBox();
+            this.Micek = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Raketa2 = new System.Windows.Forms.PictureBox();
+            this.Wall_top = new System.Windows.Forms.PictureBox();
+            this.Wall_bottom = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ovládáníToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Raketa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Micek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Raketa2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall_top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall_bottom)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Raketa
+            // 
+            this.Raketa.BackColor = System.Drawing.Color.White;
+            this.Raketa.Location = new System.Drawing.Point(12, 107);
+            this.Raketa.Name = "Raketa";
+            this.Raketa.Size = new System.Drawing.Size(10, 232);
+            this.Raketa.TabIndex = 0;
+            this.Raketa.TabStop = false;
+            // 
+            // Micek
+            // 
+            this.Micek.BackColor = System.Drawing.Color.White;
+            this.Micek.Location = new System.Drawing.Point(363, 223);
+            this.Micek.Name = "Micek";
+            this.Micek.Size = new System.Drawing.Size(30, 27);
+            this.Micek.TabIndex = 1;
+            this.Micek.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Raketa2
+            // 
+            this.Raketa2.BackColor = System.Drawing.Color.White;
+            this.Raketa2.Location = new System.Drawing.Point(775, 107);
+            this.Raketa2.Name = "Raketa2";
+            this.Raketa2.Size = new System.Drawing.Size(10, 232);
+            this.Raketa2.TabIndex = 3;
+            this.Raketa2.TabStop = false;
+            // 
+            // Wall_top
+            // 
+            this.Wall_top.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Wall_top.Location = new System.Drawing.Point(3, 3);
+            this.Wall_top.Name = "Wall_top";
+            this.Wall_top.Size = new System.Drawing.Size(792, 12);
+            this.Wall_top.TabIndex = 4;
+            this.Wall_top.TabStop = false;
+            // 
+            // Wall_bottom
+            // 
+            this.Wall_bottom.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Wall_bottom.Location = new System.Drawing.Point(-7, 539);
+            this.Wall_bottom.Name = "Wall_bottom";
+            this.Wall_bottom.Size = new System.Drawing.Size(792, 12);
+            this.Wall_bottom.TabIndex = 5;
+            this.Wall_bottom.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(271, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(423, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 39);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.ovládáníToolStripMenuItem,
+            this.toolStripMenuItem3});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(3, 428);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(152, 108);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.AutoSize = false;
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.toolStripMenuItem1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(143, 25);
+            this.toolStripMenuItem1.Text = "New Game";
+            this.toolStripMenuItem1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7});
+            this.toolStripMenuItem2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(145, 25);
+            this.toolStripMenuItem2.Text = "Difficulty";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripMenuItem4.Checked = true;
+            this.toolStripMenuItem4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem4.ForeColor = System.Drawing.Color.Lime;
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(134, 26);
+            this.toolStripMenuItem4.Text = "Easy";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripMenuItem5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(134, 26);
+            this.toolStripMenuItem5.Text = "Normal";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripMenuItem6.ForeColor = System.Drawing.Color.Red;
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(134, 26);
+            this.toolStripMenuItem6.Text = "Hard";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripMenuItem7.ForeColor = System.Drawing.Color.Purple;
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(134, 26);
+            this.toolStripMenuItem7.Text = "Insane";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // ovládáníToolStripMenuItem
+            // 
+            this.ovládáníToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ovládáníToolStripMenuItem.Name = "ovládáníToolStripMenuItem";
+            this.ovládáníToolStripMenuItem.Size = new System.Drawing.Size(145, 25);
+            this.ovládáníToolStripMenuItem.Text = "Ovládání";
+            this.ovládáníToolStripMenuItem.Click += new System.EventHandler(this.ovládáníToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(145, 25);
+            this.toolStripMenuItem3.Text = "Quit";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(210, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(352, 55);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Game paused.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(5, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 39);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Player1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(653, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 39);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Player2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(797, 551);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Wall_bottom);
+            this.Controls.Add(this.Wall_top);
+            this.Controls.Add(this.Raketa2);
+            this.Controls.Add(this.Micek);
+            this.Controls.Add(this.Raketa);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Tetris";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Raketa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Micek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Raketa2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall_top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Wall_bottom)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Raketa;
+        private System.Windows.Forms.PictureBox Micek;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Raketa2;
+        private System.Windows.Forms.PictureBox Wall_top;
+        private System.Windows.Forms.PictureBox Wall_bottom;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem ovládáníToolStripMenuItem;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int pohyb_x = 10; //Pohyb míčku doprava/doleva
+        int pohyb_y = 10; //Pohyb míčku dolu/nahoru
+        int rychlost_rakety = 15; //Rychlost pohybu naší rakety
+        bool key_up, key_down, key_w, key_s; //Proměnné pro pohyb
+        int score_player1 = 0; //skóre hráč1
+        int score_player2 = 0; //skóre hráč2
+        bool menu = true; //menu otevřené true/false
+        private void Check(object sender) // Zaškrtne správnou volbu
+        {
+            toolStripMenuItem4.Checked = false;
+            toolStripMenuItem5.Checked = false;
+            toolStripMenuItem6.Checked = false;
+            toolStripMenuItem7.Checked = false;
+           ToolStripMenuItem mI = (ToolStripMenuItem)sender;
+           mI.Checked = true;
+        }
+        private void Umisti() //Rozmístí podle velikosti obrazu
+        {
+            label1.Location = new Point((ClientSize.Width / 2) - label1.Width * 2, label1.Height);
+            label2.Location = new Point((ClientSize.Width / 2) + label2.Width, label2.Height);
+            label3.Location = new Point((ClientSize.Width / 2) - label3.Width / 2, (ClientSize.Height / 2) - label2.Height / 2);
+            label5.Location = new Point(ClientSize.Width - label5.Width, Wall_top.Height);
+
+            Wall_top.Location = new Point(0, 0);
+            Wall_bottom.Location = new Point(0, ClientSize.Height - 1);
+            Wall_top.Height = 1;
+            Wall_top.Width = ClientSize.Width;
+            Wall_bottom.Height = 1;
+            Wall_bottom.Width = ClientSize.Width;
+
+            menuStrip1.Location = new Point(0, ClientSize.Height - menuStrip1.Height);
+
+        }
+        private void Reset() // Vrátí do úvodní pozice
+        {
+            Raketa.Location = new Point(Raketa.Width, (ClientSize.Height / 2) - (Raketa.Height / 2));
+            Raketa2.Location = new Point(ClientSize.Width - (Raketa2.Width * 2), (ClientSize.Height / 2) - (Raketa2.Height / 2));
+            Micek.Location = new Point((ClientSize.Width / 2) - (Micek.Width / 2), (ClientSize.Height / 2) - (Micek.Height / 2));
+            
+            timer2.Start(); //Handle pohybu
+        }
+        private void Rozhoz()//Random rozhoz míčku
+        {
+            Random rand = new Random();
+            if (rand.Next(0, 2) == 1)
+                pohyb_x = -pohyb_x;
+            Thread.Sleep(1000);
+        }
+        private void Score_player1()//Zaskóruje hráč2
+        {
+            score_player1++;
+            label1.Text = score_player1.ToString();
+            if (score_player1 == 10)
+                WinPlayer1();
+            Reset();
+            Rozhoz();
+        }
+        private void WinPlayer1()
+        {
+            timer1.Stop();
+            MessageBox.Show("Vyhrál Player1!");
+            toolStripMenuItem1_Click(null, null);
+        }
+        private void WinPlayer2()
+        {
+            timer1.Stop();
+            MessageBox.Show("Vyhrál Player2!");
+            toolStripMenuItem1_Click(null, null);
+        }
+        private void Score_player2()//Zaskóruje hráč2
+        {
+            score_player2++;
+            label2.Text = score_player2.ToString();
+            if (score_player2 == 10)
+                WinPlayer2();
+            Reset();
+            Rozhoz();
+        }
+        private void PlaySound(string sound)
+        {
+            SoundPlayer sp = new SoundPlayer(@"sounds\" + sound);
+            sp.Play();
+        }
+        private void timer1_Tick(object sender, EventArgs e) //Pohybuje
+        {
+            Micek.Left += pohyb_x;
+            Micek.Top += pohyb_y; //Pohyb míčku
+
+            if (Micek.Bounds.IntersectsWith(Raketa.Bounds) || Micek.Bounds.IntersectsWith(Raketa2.Bounds)) //Kolize s raketama
+            {
+                pohyb_x = -pohyb_x; //obrácení směru x
+                PlaySound("plop.wav");
+            }
+            if (Micek.Bounds.IntersectsWith(Wall_bottom.Bounds) || Micek.Bounds.IntersectsWith(Wall_top.Bounds)) //Kolize se stěnama
+            {
+                pohyb_y = -pohyb_y; //obrácení směru y
+                PlaySound("beep.wav");
+            }
+
+            if (Micek.Location.X < 0)
+            { Score_player2();PlaySound("end.wav");}
+            if(Micek.Location.X >ClientSize.Width)
+            {  Score_player1();PlaySound("end.wav");}
+            
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) //Stisk klávesy
+        {
+            if (e.KeyCode == Keys.W)
+                key_w = true;
+            if (e.KeyCode == Keys.S)
+                key_s = true;
+            if (e.KeyCode == Keys.Up)
+                key_up = true;
+            if (e.KeyCode == Keys.Down)
+                key_down = true;
+
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e) //Puštění klávesy
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                if (menu)
+                    menu = false;
+                else
+                    menu = true;
+                ShowMenu(menu); 
+            }
+
+            if (e.KeyCode == Keys.W)
+                key_w = false;
+            if (e.KeyCode == Keys.S)
+                key_s = false;
+            if (e.KeyCode == Keys.Up)
+                key_up = false;
+            if (e.KeyCode == Keys.Down)
+                key_down = false;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e) //New game
+        {
+            label1.Text = "0";
+            label2.Text = "0";
+            score_player1 = 0;
+            score_player2 = 0;
+            Reset();
+            menu = false;
+            ShowMenu(menu);
+
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e) //Exit
+        {
+            Application.Exit();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)//Easy
+        {
+            pohyb_x = 10;
+            pohyb_y = 10;
+            Raketa.Height = 232;
+            Raketa2.Height = 232;
+            Check(sender);
+
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)//Normal
+        {
+            pohyb_x = 13;
+            pohyb_y = 13;
+            Raketa.Height = 180;
+            Raketa2.Height = 180;
+            Check(sender);
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)//Hard
+        {
+            pohyb_x = 16;
+            pohyb_y = 16;
+            Raketa.Height = 180;
+            Raketa2.Height = 180;
+            Check(sender);
+            
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)//Insane
+        {
+            pohyb_x = 10;
+            pohyb_y = 10;
+            Raketa.Height = 80;
+            Raketa2.Height = 80;
+            Check(sender);
+
+        }
+        private void timer2_Tick(object sender, EventArgs e) //Hýbání raketama
+         {
+             if (key_w && Raketa.Top > 0)
+                 Raketa.Top -= rychlost_rakety;
+             if (key_up && Raketa2.Top > 0)
+                 Raketa2.Top -= rychlost_rakety;
+
+             if (key_s && (Raketa.Top - ClientSize.Height) + Raketa.Height < 0)
+                 Raketa.Top += rychlost_rakety;
+             if (key_down && (Raketa2.Top - ClientSize.Height) + Raketa.Height < 0)
+                 Raketa2.Top += rychlost_rakety;
+         }
+
+         private void Form1_Load(object sender, EventArgs e)
+         {
+             Umisti();
+            Reset();
+            ShowMenu(menu);
+            label3.Visible = false;
+         } //Pozicuje a zavolá menu
+        private void ShowMenu(bool status) //Zobrazí/vypne menu
+        {
+            if (status)
+            {
+                menuStrip1.Visible = true;
+                label3.Visible = true;
+                timer1.Stop();
+                timer2.Stop();
+            }
+            else
+            {
+                menuStrip1.Visible = false;
+                label3.Visible = false;
+                timer1.Start();
+                timer2.Start();
+            }
+
+        }
+         private void Form1_Paint(object sender, PaintEventArgs e)
+         {
+             Graphics g = e.Graphics;
+             Pen pen = new Pen(Brushes.White, 1);
+             pen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+             g.DrawLine(pen, ClientSize.Width / 2, 0, ClientSize.Width / 2, ClientSize.Height); //Tečkovaná čára uprostřed
+         }
+
+         private void ovládáníToolStripMenuItem_Click(object sender, EventArgs e)//Ovládání
+         {
+             MessageBox.Show("Player1\n W = Nahoru\n S = Dolu\nPlayer2\n Šipka nahoru = Nahoru\n Šipka dolu = dolu","Ovládání");
+         }
+    }
+}
